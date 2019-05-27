dff568f35c5a64d7a9da63d3903209da018c335b
Daniel Millan <44685856+Danielmill33@users.noreply.github.com>
2019-05-26 23:21:44 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+    public static void main(String args[]) {
+        
+        System.out.print("Pace University");
+        System.out.println("CS623 DBMS Project Part 2");
+        System.out.print("Jose Daniel Millan Vega");
+        
+    }
+
+}
